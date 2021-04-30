faced9dfe459205aa8400a3b52316982b5292f6d
Vinod Gowda <62090365+vinod-gowda@users.noreply.github.com>
2021-04-30 17:07:40 +0530

    initial commit

---
+++ pages/excersise-01.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="center">
+    <div class="box">
+      <div class="box__num">Login</div>
+      <div class="box__ele">Login to access your dashboard.</div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+body {
+  background: #f2f2f2;
+  font-family: 'Poppins';
+}
+
+div {
+  margin: 1px;
+}
+
+.center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+}
+
+.box {
+  width: 300px;
+  height: 100px;
+  border-radius: 5px;
+  color: #6e6e6e;
+  background: white;
+
+  &__ele {
+    font-size: 11px;
+    text-align: left;
+    padding-left: 20px;
+    background: white;
+  }
+
+  &__num {
+    margin-top: 25px;
+    font-size: 20px;
+    font-weight: bold;
+    text-align: left;
+    line-height: 1.4;
+    padding-left: 20px;
+    background: white;
+  }
+}
+
+.center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+}
+
+.box {
+  width: 300px;
+  height: 100px;
+  border-radius: 5px;
+  color: #6e6e6e;
+  background: white;
+
+  &__ele {
+    font-size: 11px;
+    text-align: left;
+    padding-left: 20px;
+    background: white;
+  }
+
+  &__num {
+    margin-top: 25px;
+    font-size: 20px;
+    font-weight: bold;
+    text-align: left;
+    line-height: 1.4;
+    padding-left: 20px;
+    background: white;
+  }
+}
+</style>
