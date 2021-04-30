c18b277d8f43aeb8e7ae394a66a7e695fba32db5
Vinod Gowda <62090365+vinod-gowda@users.noreply.github.com>
2021-04-30 22:52:04 +0530

    line break

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,7 @@
   <div>
     <div class="nav">
       <nuxt-link to="/">Home</nuxt-link>
+      <div></div>
       <nuxt-link to="/excersise-01">Excersise 01</nuxt-link>
     </div>
     <Nuxt />
@@ -9,16 +10,15 @@
 </template>
 
 <style lang="scss">
-@import url('http//fonts.googleapis.com/css2?family=Poppins&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
 
 * {
   margin: 0;
-  font-family: 'Poppins';
+  font-family: 'Poppins', sans-serif;
 }
 .nav {
   position: fixed;
   top: 10px;
   left: 10px;
-  z-index: 100;
 }
 </style>
